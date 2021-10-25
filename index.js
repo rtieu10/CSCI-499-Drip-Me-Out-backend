50db50b6e40b5a030f756959f06ee838bd64579c
HashirAKhan <hkhan6265@bths.edu>
2021-10-24 23:17:44 -0400

    cleaned up login/signup code

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const url = require('url');
 
 
-const { api_key } = require('./credential.json');
+// const { api_key } = require('./credential.json');
 
 Parse.initialize("8dTo5v19t0vWVBB4OpdmD3g7EWSGx0P93kQxQQZ1","YA2lm6qSHNHU72qWeoTwKUXC3rGIHlhMCYqcG05W","jQWPaIzUhciZlOvs8fm8Jweo2E83ESlktBX29kWe")
 Parse.serverURL = "https://parseapi.back4app.com/"
@@ -14,6 +14,7 @@ Parse.serverURL = "https://parseapi.back4app.com/"
 let zipcode = "";
 
 const server = http.createServer();
+
 
 server.on("listening", () => {});
 
@@ -200,106 +201,62 @@ server.on("request", function(req, res) {
 
 		}
 	}
+
 	else if (req.method === "POST" && req.url.startsWith("/signup"))
 	{
-		let email = "";
-		let password = "";
-
-		let login_info = "";
-		let body = '';
+		let body;
 		req.on('data', function (data) {
-			body += data;
+			body = JSON.parse(data.toString('utf8'));
+			console.log(body);
 		});
 		req.on('end', function () {
-			login_info = body;
-			for (let i = 0; i < login_info.length; i++)
-			{
-				if (login_info[i] === "&")
-				{
-					email = login_info.substr(0, i);
-					password = login_info.substr(i + 1, login_info.length);
-					break;
-				}
-			}
-
-			user_search(email, password, res);
+			parseUserSignup(body["username"], body["email"], body["password"], res);
 		});
 	}
+
+
 	else if (req.method === "POST" && req.url.startsWith("/login"))
 	{
-		let login_info = "";
-		let body = '';
+		let body;
 		req.on('data', function (data) {
-			body += data;
+			body = JSON.parse(data.toString('utf8'));
+			console.log(body);
 		});
 		req.on('end', function () {
-			login_info = body;
-			for (let i = 0; i < login_info.length; i++)
-			{
-				if (login_info[i] === "&")
-				{
-					email = login_info.substr(0, i);
-					password = login_info.substr(i + 1, login_info.length);
-					break;
-				}
-			}
-			let loginpage = true;
-			user_search(email, password, res, loginpage);
+			parseUserLogin(body["email"], body["password"], res);
 		});
 	}
 });
 
-function user_search(email, password, res, loginpage)
-{
-	const Users = Parse.Object.extend("Users");
-	const query = new Parse.Query(Users);
-	async function find_user() {
-		const results = await query.find();
-		let i = 0;
-		let recorded = false;
-		function check_user()
-		{
-			query.get(results[i].id).then((user) => {
-				let db_email = user.get("email");
-				let db_password = user.get("password");
-				if (db_email === email && db_password === password)
-				{
-					let db_email = user.get("email");
-					let db_password = user.get("password");
+async function parseUserSignup(usern, email, pass, res){
+	const user = new Parse.User();
+	user.set("username", usern);
+	user.set("password", pass);
+	user.set("email", email);
 
-					res.write("verified");
-					res.end();
-					recorded = true;
-				}
-				if (i < results.length - 1)
-				{
-					i++;
-					check_user();
-				}
-				else 
-				{
-					if (!recorded && !loginpage)
-					{
-						let user_to_db = new Parse.Object("Users");
-						user_to_db.set("email", email);
-						user_to_db.set("password", password);
-						user_to_db.save();
-						res.write("signedup")
-						res.end();
-					}
-					if (loginpage)
-					{
-						res.write("unsuccessful");
-						res.end();
-					}
-				}
-			}, (error) => {
-				console.log('user not retrieved, error');
-			});
-		}
-		check_user();
-	};
-	find_user();
+	try {
+		await user.signUp();
+		// Hooray! Let them use the app now.
+		res.write("signedup")
+		res.end();
+	} catch (error) {
+		// Show the error message somewhere and let the user try again.
+		console.log("Error: " + error.code + " " + error.message);
+		res.write("unsuccessful");
+		res.end();
+	}
+}
+
+async function parseUserLogin(email, pass, res){
+	try{
+		const user = await Parse.User.logIn(email, pass);
+		res.write("verified")
+		res.end();
+	} catch (error) {
+		console.log("Error: " + error.code + " " + error.message);
+		res.write("unsuccessful");
+		res.end();
+	}
 }
 
 server.listen(8080);
@@ -327,17 +284,17 @@ let weather_status = "";
 let low = "";
 let high = "";
 
-function get_current_weather(res){
-	let endpoint = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=${api_key}`;
-	https.request(`${endpoint}`, {method:"GET"}, process_stream).end();
-	function process_stream(weather_stream){
-		let weather_data = "";
-		weather_stream.on("data", chunk => weather_data += chunk);
-		weather_stream.on("end", () => {
-			serve_results(weather_data, res);
-		});
-	}
-}
+// function get_current_weather(res){
+// 	let endpoint = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=${api_key}`;
+// 	https.request(`${endpoint}`, {method:"GET"}, process_stream).end();
+// 	function process_stream(weather_stream){
+// 		let weather_data = "";
+// 		weather_stream.on("data", chunk => weather_data += chunk);
+// 		weather_stream.on("end", () => {
+// 			serve_results(weather_data, res);
+// 		});
+// 	}
+// }
 
 function serve_results(weather_data, res){
 	weather = JSON.parse(weather_data);
