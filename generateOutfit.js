f5780417af2e6ad8c4e97099ed4343a6849495e2
rtieu10 <rachel.tieu02@myhunter.cuny.edu>
2021-10-28 15:01:19 -0400

    completed fetchParse function to retrieve user's clothing

--- generateOutfit.js
+++ generateOutfit.js
@@ -1,3 +1,6 @@
+const Parse = require('parse/node');
+Parse.initialize("8dTo5v19t0vWVBB4OpdmD3g7EWSGx0P93kQxQQZ1","YA2lm6qSHNHU72qWeoTwKUXC3rGIHlhMCYqcG05W","jQWPaIzUhciZlOvs8fm8Jweo2E83ESlktBX29kWe")
+Parse.serverURL = "https://parseapi.back4app.com/"
 
 function generateOutfit(data, res){
   fetchParse();
@@ -8,7 +11,25 @@ function generateOutfit(data, res){
 }
 
 //get clothing items for the user
-function fetchParse(user){
+//reference addItem.js run to check the duplicate function
+//user: a string containing user email (ex: rachel@test.com)
+async function fetchParse(user){
+  //use parse query to get the users email
+  //pull in the users database of clothing that they have uploaded
+  //save the clothing items that the user has uploaded as an array
+
+  //connecting to ClothingItem table in parse
+  const ClothingItem = Parse.Object.extend("ClothingItem");
+  //querying the ClothingItem table
+  const query = new Parse.Query(ClothingItem);
+  //check if the email column of the table equals to the user's email
+  query.equalTo("email", user)
+  //waits for the results array to be populated with the data that meets the conditions
+  const results = await query.find()
+  //alert that the request was successful
+  alert("Successfully retrieved " + results.length + "items of clothing.")
+  //return results array that stores all of the rows that met query condition
+  return results
 
 }
 
@@ -33,6 +54,6 @@ function pickClothing(){
 }
 
 //pass through the catagory to pick random item that complies with the users filters
-function pickFilteredItem(catagory){
+function pickFilteredItem(category){
 
 }
