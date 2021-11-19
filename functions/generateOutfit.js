0ff919165b9c954f5f9e52d977b9a63e4b401fed
Michael Wong <michaelwong1391@gmail.com>
2021-11-19 15:09:35 -0500

    changed type to category for coats when generating outfits

--- functions/generateOutfit.js
+++ functions/generateOutfit.js
@@ -61,7 +61,7 @@ function organizeCategories(arrClothings){
 
   //iterate through array of clothing, and sort the items into respective arrays
   for(let i = 0; i < arrClothings.length; i++){
-    if(arrClothings[i].get('type') == 'Coats'){
+    if(arrClothings[i].get('category') == 'Coats'){
       pushItem(arrClothings[i], coats);
     }
     else if(arrClothings[i].get('category') == 'Hoodies/Sweaters/Jackets'){
