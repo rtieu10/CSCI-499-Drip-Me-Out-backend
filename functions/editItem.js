5c4ef694c9f0eed85dc3715b5f57d1a57902def5
Michael Wong <michaelwong1391@gmail.com>
2021-12-05 12:57:42 -0500

    username and zip code can be updated

--- functions/editItem.js
+++ functions/editItem.js
@@ -1,25 +1,36 @@
-const Parse = require('parse/node');
-Parse.initialize("8dTo5v19t0vWVBB4OpdmD3g7EWSGx0P93kQxQQZ1","YA2lm6qSHNHU72qWeoTwKUXC3rGIHlhMCYqcG05W","jQWPaIzUhciZlOvs8fm8Jweo2E83ESlktBX29kWe")
-Parse.serverURL = "https://parseapi.back4app.com/"
+const Parse = require("parse/node");
+Parse.initialize(
+  "8dTo5v19t0vWVBB4OpdmD3g7EWSGx0P93kQxQQZ1",
+  "YA2lm6qSHNHU72qWeoTwKUXC3rGIHlhMCYqcG05W",
+  "jQWPaIzUhciZlOvs8fm8Jweo2E83ESlktBX29kWe"
+);
+Parse.serverURL = "https://parseapi.back4app.com/";
 
-
-async function editItem(data, res){
-  let image = data["image"].substr("data:image/jpeg;base64,".length, data["image"].length - "data:image/jpeg;base64,".length);
-  if (data["image"].substr(12, 3) === "png")
-  {
-    image = data["image"].substr("data:image/png;base64,".length, data["image"].length - "data:image/png;base64,".length);
+async function editItem(data, res) {
+  let image = "";
+  if (data["image"]?.substr(12, 4) === "jpeg") {
+    data["image"].substr(
+      "data:image/jpeg;base64,".length,
+      data["image"].length - "data:image/jpeg;base64,".length
+    );
+  }
+  if (data["image"]?.substr(12, 3) === "png") {
+    image = data["image"].substr(
+      "data:image/png;base64,".length,
+      data["image"].length - "data:image/png;base64,".length
+    );
   }
   duplicate = await checkDuplicate(data, image, res);
   console.log(`duplicate = ${duplicate}`);
-  if(!(duplicate)){
+  if (!duplicate) {
     const ClothingItem = Parse.Object.extend("ClothingItem");
     const query = new Parse.Query(ClothingItem);
     query.equalTo("email", data["email"]);
-    try{
+    try {
       const results = await query.find();
       for (let i = 0; i < results.length; i++) {
         const object = results[i];
-        if(object.id === data["id"]){
+        if (object.id === data["id"]) {
           object.set("name", data["label"]);
           object.set("imagedata", image);
           object.set("category", data["category"]);
@@ -45,11 +56,14 @@ async function checkDuplicate(data, image, res) {
   const ClothingItem = Parse.Object.extend("ClothingItem");
   const query = new Parse.Query(ClothingItem);
   query.equalTo("email", data["email"]);
-  try{
+  try {
     const results = await query.find();
     for (let i = 0; i < results.length; i++) {
       const object = results[i];
-      if(object.get("name") === data["label"] && image === object.get("imagedata")){
+      if (
+        object.get("name") === data["label"] &&
+        image === object.get("imagedata")
+      ) {
         res.write("recorded");
         res.end();
         return true;
